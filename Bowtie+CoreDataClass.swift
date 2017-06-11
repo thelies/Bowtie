079aebe5924531e21e97d6854b393986ffc187f4
Le Ngoc Hoan <le-ngoc-hoan@altplus.co.jp>
2017-06-11 23:23:22 +0700

    Init Project

---
+++ Bowtie+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Bowtie+CoreDataClass.swift
+//  Bow Ties
+//
+//  Created by Le Ngoc Hoan on 6/11/17.
+//  Copyright © 2017 Razeware. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+public class Bowtie: NSManagedObject {
+
+}
